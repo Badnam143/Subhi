8d642720a3a72438a360e2b7a1da1c4a6dfbf0a6
Badnam143 <154507591+Badnam143@users.noreply.github.com>
2024-03-30 13:10:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/subhichiku/Subhi",
+    "https://github.com/Badnam143/Subhi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Subhi")
 GIT_TOKEN = getenv(
